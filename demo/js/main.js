d31aa30beccb5cb354be610a73d5bd81db48d1ca
burning <iburning@live.cn>
2016-08-29 09:02:48 +0800

    temp

--- demo/js/main.js
+++ demo/js/main.js
@@ -44,14 +44,14 @@ router.map({
   '/grid': {
     component: require('./grid.vue')
   },
-  //
-  // '/list': {
-  //   component: require('./pages/list')
-  // },
-  //
-  // '/scroll-list': {
-  //   component: require('./pages/scrollList')
-  // },
+
+  '/list': {
+    component: require('./list.vue')
+  },
+
+  '/scroll-list': {
+    component: require('./scrollList.vue')
+  },
   //
   // '/tab-bar': {
   //   component: require('./pages/tabBar')
--- src/js/main.js
+++ src/js/main.js
@@ -15,8 +15,12 @@ import CILinkButton from './CIButton/CILinkButton.vue'
 import CIList from './CIList/CIList.vue'
 import CIListCell from './CIList/CIListCell.vue'
 
+import CILoading from './CILoading/CILoading.vue'
+
 import CIGrid from './CIGrid/CIGrid.vue'
 import CIGridCell from './CIGrid/CIGridCell.vue'
+
+import CIScrollHelper from './utils/CIScrollHelper'
 
 
 const components = {
@@ -30,6 +34,8 @@ const components = {
 
   'ci-list': CIList,
   'ci-list-cell': CIListCell,
+
+  'ci-loading': CILoading,
 
   'ci-grid': CIGrid,
   'ci-grid-cell': CIGridCell
