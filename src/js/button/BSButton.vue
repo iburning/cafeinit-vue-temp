98ddc98e3cc9d4b8e791fa60763ed53c704d3f37
burning <iburning@live.cn>
2016-09-11 17:25:42 +0800

    整理代码

--- src/js/button/BSButton.vue
+++ src/js/button/BSButton.vue
@@ -1,63 +1,12 @@
-<template>
-  <button class="btn" type="{{type}}"
-    v-bind:class="[
-      'btn-' + style,
-      size ? ('btn-' + size) : '',
-      radius ? 'radius' : '',
-      round ? 'round' : '',
-      (status === 'actived') ? 'active' :'',
-      block ? 'btn-block' : ''
-    ]"
-    v-bind:disabled="status === 'disabled' || status === 'loading'">
-    <span class="icon-circle-o-notch icon-spin" v-if="status === 'loading'"></span>
-    <slot>This is a button</slot>
-  </button>
-</template>
-
+<template src="./_button.html"></template>
 
 <script>
-export default {
-  props: {
-    type: {
-      type: String,
-      default: 'button'
-    },
+import config from './_button'
 
-    style: {
-      type: String,
-      default: 'default'
-    },
-
-    size: {
-      type: String,
-      default: ''
-    },
-
-    block: {
-      type: String,
-      default: ''
-    },
-
-    radius: {
-      type: String,
-      default: ''
-    },
-
-    round: {
-      type: String,
-      default: ''
-    },
-
-    status: {
-      type: String,
-      default: ''
-    }
-  },
-
-  methods: {
-    click: function () {
-      this.$emit('click');
-    }
-  }
+config.props.ns = {
+  type: String,
+  default: ''
 }
+
+export default config
 </script>
---
+++ src/js/button/_button.js
@@ -0,0 +1,50 @@
+/**
+ * @fileOverview button
+ * @author: burning <www.cafeinit.com>
+ * @date: 2016-09-11
+ */
+
+export default {
+  props: {
+    type: {
+      type: String,
+      default: 'button'
+    },
+
+    style: {
+      type: String,
+      default: ''
+    },
+
+    size: {
+      type: String,
+      default: ''
+    },
+
+    block: {
+      type: String,
+      default: ''
+    },
+
+    radius: {
+      type: String,
+      default: ''
+    },
+
+    round: {
+      type: String,
+      default: ''
+    },
+
+    status: {
+      type: String,
+      default: ''
+    }
+  },
+
+  methods: {
+    click() {
+      this.$dispatch('click')
+    }
+  }
+}
